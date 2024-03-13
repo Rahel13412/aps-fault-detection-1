03a20e6931e62f36ec52902fbfc2265bce67c723
Rahel13412 <rahelali13412@gmail.com>
2024-03-13 11:31:11 +0530

    added main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from sensor.logger import logging
+from sensor.exception import SensorException
+import sys,os
+
+
+
+
+
+if __name__=="__main__":
+     try:
+          pass
+     except Exception as e:
+          print(e)
