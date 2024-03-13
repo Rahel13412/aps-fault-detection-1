f8e12702e4941ec8de673998df6cf2ed53947f91
Rahel13412 <rahelali13412@gmail.com>
2024-03-13 14:33:34 +0530

    data-ingestion completed

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from sensor.logger import logging
 from sensor.exception import SensorException
 from sensor.utils import get_collection_as_dataframe
 import sys,os
+from sensor.entity import config_entity
+from sensor.components.data_ingestion import DataIngestion
 
 
 
@@ -9,7 +11,11 @@ import sys,os
 
 if __name__=="__main__":
      try:
-          get_collection_as_dataframe()
+          training_pipeline_config = config_entity.TrainingPipelineConfig()
+          data_ingestion_config  = config_entity.DataIngestionConfig(training_pipeline_config=training_pipeline_config)
+          print(data_ingestion_config.to_dict())
+          data_ingestion = DataIngestion(data_ingestion_config=data_ingestion_config)
+          print(data_ingestion.initiate_data_ingestion())
      except Exception as e:
           print(e)
-     
+      
